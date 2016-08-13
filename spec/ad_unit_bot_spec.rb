3b80f148dd7234321bcf236e7a76ffef07adfedf
Tanya Novitskaya <Tanyanvt@gmail.com>
2016-08-13 15:18:26 +0300

    Add spec for login

--- spec/ad_unit_bot_spec.rb
+++ spec/ad_unit_bot_spec.rb
@@ -1,11 +1,29 @@
 require 'spec_helper'
 
+# describe AdUnitBot do
+#   it 'has a version number' do
+#     expect(AdUnitBot::VERSION).not_to be nil
+#   end
+
+#   it 'does something useful' do
+#     expect(false).to eq(true)
+#   end
+# end
+
 describe AdUnitBot do
-  it 'has a version number' do
-    expect(AdUnitBot::VERSION).not_to be nil
+  let(:email_user) { 'tanuwkanov@yandex.ru' }
+  let(:password) { '123456q' }
+
+  it "logs in with valid credentials" do
+    expect { AdUnitBot.headlessly_login }.to_not raise_error
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  context "logs in with invalid credentials" do
+    let(:account_user) { "dizwarhomo" }
+    let(:password)     { "homohomo" }
+
+    it "raises LoginFailed" do
+      expect { AdUnitBot.headlessly_login }.to raise_error "Invalid login or password"
+    end
   end
 end
