d4724a2a27cd0b6904097168105c21898b7bd509
Tanya Novitskaya <Tanyanvt@gmail.com>
2016-08-15 22:02:59 +0300

    Add creat ad unit spec and method

--- lib/ad_unit_bot.rb
+++ lib/ad_unit_bot.rb
@@ -4,7 +4,6 @@ require 'headless'
 require 'selenium-webdriver'
 require 'capybara'
 require 'byebug'
-
 require 'capybara/dsl'
 
 Capybara.run_server = false
@@ -12,39 +11,102 @@ Capybara.current_driver = :selenium
 
 class AdUnitBot
   include Capybara::DSL
+  include RSpec::Matchers
+  WAIT = Selenium::WebDriver::Wait.new(:timeout => 30)
 
-  def initialize(email, password)
+  def initialize(email, password, title)
     @email = email
     @password = password
+    @title = title
   end
 
   def login
     visit "http://target.my.com/"
-    sleep(5)
+
+    process_after_visit_site
+    raise "Invalid login or password" unless logged_in?
+  end
+
+  def choose_platform(platform_id)
+    visit "http://target.my.com/"
+    process_after_visit_site
+    WAIT.until { has_link?("Создать площадку") }
+    begin
+      platform = find(:xpath, "//a[contains(@href, '#{platform_id}')]")
+    rescue Capybara::ElementNotFound
+      raise "Platform id is not found."
+    end
+
+    platform.click
+  end
+
+  def verifies_platform?(platform_id)
+    choose_platform(platform_id)
+    WAIT.until { has_link?("Создать блок") }
+    find(:xpath, "//a[contains(@href, '#{platform_id}/edit')]")
+  end
+
+  def create_ad_unit(platform_id)
+    verifies_platform?(platform_id)
+    click_on("Создать блок")
+    WAIT.until { has_content?("Типы блоков") }
+    fill_in 'Назовите ваш блок', with: @title
+    type = find(:xpath, "//span[text()='medium']")
+    type.click
+    show = find(:xpath, "//div[span[text()='Частота показов:']]") unless
+      has_content? ("Лимит показов уникальному пользователю")
+    show.click
+    select_limit
+    select_period
+    select_interval
+    new_unit
+  end
+
+  def select_limit
+    as = find(:xpath, "//li[span[contains(text(), 'Лимит показов уникальному пользователю')]]/select/option[.='1']")
+    as.select_option
+  end
+
+  def select_period
+    as = find(:xpath, "//li[span[contains(text(), 'за период')]]/select/option[.='в неделю']")
+    as.select_option
+  end
+
+  def select_interval
+    as = find(:xpath, "//li[span[contains(text(), 'Интервал между показами')]]/select/option[.='3']")
+    as.select_option
+  end
+
+  def new_unit
+    new_unit_button = find(:xpath, "//span[text()='Добавить блок']")
+    new_unit_button.click
+  end
+
+  def created_new_ad_unit(title)
+    WAIT.until { has_link?("Создать блок") }
+    ad_unit = find(:xpath, "//table/tbody/tr/td/div/a[text()='#{title}']")
+  rescue Capybara::ElementNotFound
+    raise "New ad unit not found" 
+  end
+
+  private
+
+  def process_after_visit_site
+    WAIT.until { has_xpath?("//span[text()='Войти']") }
     auth_button = find(:xpath, "//span[text()='Войти']")
     auth_button.click
 
     fill_in 'Email или телефон', with: @email
     fill_in 'Пароль', with: @password
+
+    WAIT.until { has_button?('Войти') }
     click_button "Войти"
-
-    raise "Invalid login or password" unless logged_in?
   end
-
-  def choose_platform(platform_id)
-    find(:xpath, "//a[contains(@href, '#{platform_id}')]").click
-  end
-
-  def verifies_platform?(platform_id)
-    login
-    choose_platform(platform_id)
-    find(:xpath, "//a[contains(@href, '#{platform_id}/edit')]")
-  end
-
-  private
 
   def logged_in?
-    sleep(5)
-    has_link? "Создать площадку"
+    WAIT.until { has_xpath?("//span[text()='#{@email}']") }
+    has_xpath?("//span[text()='#{@email}']")
   end
 end
+
+ 
--- spec/ad_unit_bot_spec.rb
+++ spec/ad_unit_bot_spec.rb
@@ -22,9 +22,8 @@ describe AdUnitBot do
   let(:platform_id) { '300029472' }
 
   it "logs in with valid credentials" do
-    unit = AdUnitBot.new('tanuwkanov@yandex.ru', '123456q')
+    unit = AdUnitBot.new('tanuwkanov@yandex.ru', '123456q', 1)
     expect { unit.login }.to_not raise_error
-    # expect { ad_unit.headlessly_login }.to_not raise_error
   end
 
   context "logs in with invalid credentials" do
@@ -32,13 +31,30 @@ describe AdUnitBot do
     let(:password)     { "homohomo" }
 
     it "raises LoginFailed" do
-      unit = AdUnitBot.new('dizwarhomo@ert.ty', 'homohomo')
+      unit = AdUnitBot.new('dizwarhomo@ert.ty', 'homohomo', 2)
       expect { unit.login }.to raise_error(RuntimeError)
     end
   end
 
   it "verifies a platform is valid" do
-    unit = AdUnitBot.new('tanuwkanov@yandex.ru', '123456q')
-    expect(unit.verifies_platform?(10165)).to be_truthy 
+    unit = AdUnitBot.new('tanuwkanov@yandex.ru', '123456q', 2)
+    expect(unit.verifies_platform?('10165')).to be_truthy 
+  end
+
+  it "platform is not found" do
+    unit = AdUnitBot.new('tanuwkanov@yandex.ru', '123456q', 3)
+    expect { unit.choose_platform('fail') }.to raise_error("Platform id is not found.")
+  end
+
+  it "creates ad unit" do
+    unit = AdUnitBot.new('tanuwkanov@yandex.ru', '123456q', 112)
+    unit.create_ad_unit('10165')
+    expect { unit.created_new_ad_unit('111') }.to_not raise_error
+  end
+
+  it "new ad unit did not create" do
+    unit = AdUnitBot.new('tanuwkanov@yandex.ru', '123456q', 112)
+    unit.create_ad_unit('10165')
+    expect { unit.created_new_ad_unit('113') }.to raise_error("New ad unit not found")
   end
 end
